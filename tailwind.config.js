9ba08ea3136e5a39ff57dc5b68d55a59524ddc22
suha <suhach0523@gmail.com>
2024-07-03 21:57:44 +0900

    chore: 초기 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // 파일 경로를 프로젝트 구조에 맞게 수정
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
