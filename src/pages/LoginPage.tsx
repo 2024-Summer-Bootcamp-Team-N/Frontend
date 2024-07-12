dd77115908245967d10f504098b6b4a4d3d5ae96
권혁진 <rnjsgurwls123@naver.com>
2024-07-12 14:34:19 +0900

    /design: 로그인페이지

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import Signup from '../components/Signup';
+import Login from '../components/Login';
+import Main from '../assets/img/Main.svg'; // 배경 이미지 import
+import LogoWhite from '../assets/img/LogoWhite.svg'; // 로고 이미지 import
+
+function LoginPage() {
+  const [showSignupModal, setShowSignupModal] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(true); // 페이지 로드 시 로그인 모달 표시
+
+  useEffect(() => {
+    setShowLoginModal(true); // 페이지 로드 시 로그인 모달 표시
+  }, []);
+
+  const handleSignupClick = () => {
+    setShowLoginModal(false);
+    setShowSignupModal(true);
+  };
+  return (
+    <div
+      className="w-[1920px] h-[1080px] overflow-hidden bg-white font-nanumSquareR bg-cover bg-center"
+      style={{ backgroundImage: `url(${Main})`, backgroundSize: 'auto' }}
+    >
+      {/* 로고 이미지 설정 */}
+      <div className="absolute top-20 left-0 right-0 flex justify-center items-center p-4">
+        <img src={LogoWhite} alt="Logo" className="w-[671px] h-[auto]" />
+        {/* 회원가입 및 로그인 버튼 */}
+      </div>
+      {/* 모달 창 */}
+      {showSignupModal && <Signup onClose={() => setShowSignupModal(false)} />}
+      {showLoginModal && <Login onSignupClick={handleSignupClick} onClose={() => setShowLoginModal(false)} />}
+    </div>
+  );
+}
+
+export default LoginPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // 파일 경로를 프로젝트 구조에 맞게 수정
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // NanumSquare 폰트
+        nanumSquareB: ['NanumSquareB', 'sans-serif'],
+        nanumSquareEB: ['NanumSquareEB', 'sans-serif'],
+        nanumSquareL: ['NanumSquareL', 'sans-serif'],
+        nanumSquareR: ['NanumSquareR', 'sans-serif'],
+        // NanumSquareRound 폰트
+        nanumSquareRoundB: ['NanumSquareRoundB', 'sans-serif'],
+        nanumSquareRoundEB: ['NanumSquareRoundEB', 'sans-serif'],
+        nanumSquareRoundL: ['NanumSquareRoundL', 'sans-serif'],
+        nanumSquareRoundR: ['NanumSquareRoundR', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
