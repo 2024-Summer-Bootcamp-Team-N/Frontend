393695942175a888df382a2a9050843de5092e67
JongWan <kjongw0719@gmail.com>
2024-07-16 03:09:18 +0900

    [design] 부동산 임대차 계약서 구현

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,21 +3,21 @@ import LogoBlue from '../assets/img/LogoBlue.svg';
 
 const Navbar = () => {
   return (
-    <div className="flex w-full h-[72px] bg-white fixed top-0 left-0 z-50 flex items-center p-1.5 font-NanumSquareRoundB">
+    <div className="flex w-full h-[72px] bg-white fixed top-0 left-0 z-50 items-center p-1.5">
       <Link to="/">
         <img src={LogoBlue} alt="Logo" className="mt-0.5 mr-5" />
       </Link>
-      <div className="flex-grow"></div>
-      <div className="flex left-[-0.5px] items-center gap-4">
-        <Link to="/map" className="hover:underline text-xl text-[#49454F]">
+      <div className="flex-grow "></div>
+      <div className="flex items-center gap-4">
+        <Link to="/map" className="hover:underline text-xl text-[#49454F] font-[NanumSquareRoundB]">
           내 집 찾기
         </Link>
         <span className="border-l border-[#E0E0E0] h-[19px]"></span>
-        <Link to="/login" className="hover:underline text-xl text-[#49454F]">
+        <Link to="/login" className="hover:underline text-xl text-[#49454F] font-[NanumSquareRoundB]">
           로그인
         </Link>
         <span className="border-l border-[#E0E0E0] h-[19px]"></span>
-        <Link to="/signup" className="hover:underline text-xl text-[#49454F]">
+        <Link to="/signup" className="hover:underline text-xl text-[#49454F] font-[NanumSquareRoundB]">
           회원가입
         </Link>
       </div>
--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,8 +1,8 @@
-import Navbar from '../components/Navbar.tsx'; 
+import Paper from '../components/ContractPaper.tsx'; 
 
 const MapPage = () => {
   return <div>
-      <Navbar />
+      <Paper />
     </div>;
 };
 
