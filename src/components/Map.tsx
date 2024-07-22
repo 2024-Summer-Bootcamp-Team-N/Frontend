848dc9bef634097631fa8563ecccd809b933eec6
suha <154861396+suhach0523@users.noreply.github.com>
2024-07-23 00:02:56 +0900

    Update Map.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import MapPlus from '../assets/img/MapPlus.svg';
 import MapMinus from '../assets/img/MapMinus.svg';
 import Sidebar from './Sidebar.tsx';
