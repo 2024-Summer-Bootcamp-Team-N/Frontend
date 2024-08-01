0afc9896f29f314821dc6a8dd42d8a358b9f3b64
김민기 <mocodoco@naver.com>
2024-08-01 15:17:09 +0900

    배포환경 인증서 참조 기능

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
+    https: process.env.NODE_ENV === 'production3' ? {
+      key: fs.readFileSync(path.resolve('/etc/letsencrypt/live/houseadvisor.site/privkey.pem')),
+      cert: fs.readFileSync(path.resolve('/etc/letsencrypt/live/houseadvisor.site/fullchain.pem')),
+    } : false,
     proxy: {
       '/ws': {
         target: 'http://daphne:8001',
@@ -15,10 +20,14 @@ export default defineConfig({
         secure: false,
       },
     },
-    hmr: {
+    hmr: process.env.NODE_ENV === 'production' ? {
       host: 'houseadvisor.site',
       protocol: 'wss',
       clientPort: 443,
+    } : {
+      host: 'localhost',
+      protocol: 'ws',
+      clientPort: 5173,
     },
   },
   optimizeDeps: {
